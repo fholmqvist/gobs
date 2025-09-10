35c8d22cae6b58e74fc6eb981d2ea91d87b7b8c4
Fredrik <fredrik.d.holmqvist@gmail.com>
2025-09-10 11:57:11 +0200

    sync auto-commit: #57

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
+#include "base.hpp"
 #include "game.hpp"
 #include "levels.hpp"
+#include "timer.hpp"
 
 int main() {
+    log_info("Starting game");
+
+    Timer timer;
     Window window;
     if (!window.init()) {
         throw std::runtime_error("Window failed to initialize");
@@ -12,6 +17,8 @@ int main() {
     if (!game.init()) {
         throw std::runtime_error("Game failed to initialize");
     }
+
+    log_info("Game started in %s", time_to_string(timer.stop()).c_str());
 
     while (game.running) {
         game.update();
